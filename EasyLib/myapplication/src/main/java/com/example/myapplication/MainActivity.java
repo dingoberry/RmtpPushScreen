183b3fdfdc8ad5a78f70351bd0e1dbc05de8ed32
wangjunkai <wangjunkai@g7.com.cn>
2020-03-22 21:18:27 +0800

    change: 合入EasyLib项目，解决编译问题。
    Signed-off-by: wangjunkai <wangjunkai@g7.com.cn>

---
+++ EasyLib/myapplication/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,239 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.SurfaceTexture;
+import android.media.projection.MediaProjectionManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.lifecycle.Observer;
+
+import org.easydarwin.push.MediaStream;
+
+import io.reactivex.Single;
+import io.reactivex.functions.Consumer;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CAMERA_PERMISSION = 1000;
+    private static final int REQUEST_MEDIA_PROJECTION = 1001;
+    public static final String HOST = "cloud.easydarwin.org";
+    private MediaStream mediaStream;
+
+    private Single<MediaStream> getMediaStream() {
+        Single<MediaStream> single = RxHelper.single(MediaStream.getBindedMediaStream(this, this), mediaStream);
+        if (mediaStream == null) {
+            return single.doOnSuccess(new Consumer<MediaStream>() {
+                @Override
+                public void accept(MediaStream ms) throws Exception {
+                    mediaStream = ms;
+                }
+            });
+        } else {
+            return single;
+        }
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        CheckBox hevc_enable = findViewById(R.id.enable_265);
+        hevc_enable.setChecked(PreferenceManager.getDefaultSharedPreferences(this).getBoolean("try_265_encode", false));
+        hevc_enable.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                PreferenceManager.getDefaultSharedPreferences(MainActivity.this).edit().putBoolean("try_265_encode", isChecked).apply();
+            }
+        });
+
+        // 启动服务...
+        Intent intent = new Intent(this, MediaStream.class);
+        startService(intent);
+
+        getMediaStream().subscribe(new Consumer<MediaStream>() {
+            @Override
+            public void accept(final MediaStream ms) throws Exception {
+                ms.observeCameraPreviewResolution(MainActivity.this, new Observer<int[]>() {
+                    @Override
+                    public void onChanged(@Nullable int[] size) {
+                        Toast.makeText(MainActivity.this, "当前摄像头分辨率为:" + size[0] + "*" + size[1], Toast.LENGTH_SHORT).show();
+                    }
+                });
+                final TextView pushingStateText = findViewById(R.id.pushing_state);
+                final TextView pushingBtn = findViewById(R.id.pushing);
+                ms.observePushingState(MainActivity.this, new Observer<MediaStream.PushingState>() {
+
+                    @Override
+                    public void onChanged(@Nullable MediaStream.PushingState pushingState) {
+                        if (pushingState.screenPushing) {
+                            pushingStateText.setText("屏幕推送");
+
+                            // 更改屏幕推送按钮状态.
+
+                            TextView tview = findViewById(R.id.pushing_desktop);
+                            if (ms.isScreenPushing()) {
+                                tview.setText("取消推送");
+                            } else {
+                                tview.setText("推送屏幕");
+                            }
+                            findViewById(R.id.pushing_desktop).setEnabled(true);
+                        } else {
+                            pushingStateText.setText("推送");
+                            if (ms.isCameraPushing()) {
+                                pushingBtn.setText("停止");
+                            } else {
+                                pushingBtn.setText("推送");
+                            }
+                        }
+
+                        pushingStateText.append(":\t" + pushingState.msg);
+                        if (pushingState.state > 0) {
+                            pushingStateText.append(pushingState.url);
+                            pushingStateText.append("\n");
+                            if ("avc".equals(pushingState.videoCodec)) {
+                                pushingStateText.append("视频编码方式：" + "H264硬编码");
+                            }else if ("hevc".equals(pushingState.videoCodec)) {
+                                pushingStateText.append("视频编码方式："  + "H265硬编码");
+                            }else if ("x264".equals(pushingState.videoCodec)) {
+                                pushingStateText.append("视频编码方式："  + "x264");
+                            }
+                        }
+
+                    }
+                });
+                TextureView textureView = findViewById(R.id.texture_view);
+                if (textureView.isAvailable()) {
+                    ms.setSurfaceTexture(textureView.getSurfaceTexture());
+                } else {
+                    textureView.setSurfaceTextureListener(new SurfaceTextureListenerWrapper() {
+                        @Override
+                        public void onSurfaceTextureAvailable(SurfaceTexture surfaceTexture, int i, int i1) {
+                            ms.setSurfaceTexture(surfaceTexture);
+                        }
+
+                        @Override
+                        public boolean onSurfaceTextureDestroyed(SurfaceTexture surfaceTexture) {
+                            ms.setSurfaceTexture(null);
+                            return true;
+                        }
+                    });
+                }
+
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED ||
+                        ActivityCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{android.Manifest.permission.CAMERA, android.Manifest.permission.RECORD_AUDIO}, REQUEST_CAMERA_PERMISSION);
+                }
+            }
+        }, new Consumer<Throwable>() {
+            @Override
+            public void accept(Throwable throwable) throws Exception {
+                Toast.makeText(MainActivity.this, "创建服务出错!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void onPushing(View view) {
+        getMediaStream().subscribe(new Consumer<MediaStream>() {
+            @Override
+            public void accept(MediaStream mediaStream) throws Exception {
+                MediaStream.PushingState state = mediaStream.getPushingState();
+                if (state != null && state.state > 0) { // 终止推送和预览
+                    mediaStream.stopStream();
+                    mediaStream.closeCameraPreview();
+                } else {                                // 启动预览和推送.
+                    mediaStream.openCameraPreview();
+                    String id = PreferenceManager.getDefaultSharedPreferences(MainActivity.this).getString("caemra-id", null);
+                    if (id == null) {
+                        double v = Math.random() * 1000;
+                        id = "c_" + (int) v;
+                        PreferenceManager.getDefaultSharedPreferences(MainActivity.this).edit().putString("caemra-id", id).apply();
+                    }
+                    mediaStream.startStream(HOST, "554", id);
+                }
+            }
+        });
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CAMERA_PERMISSION: {
+                if (grantResults.length > 1
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                    getMediaStream().subscribe(new Consumer<MediaStream>() {
+                        @Override
+                        public void accept(MediaStream mediaStream) throws Exception {
+                            mediaStream.notifyPermissionGranted();
+                        }
+                    });
+                } else {
+                    finish();
+                }
+                break;
+            }
+        }
+    }
+
+    // 推送屏幕.
+    public void onPushScreen(final View view) {
+        getMediaStream().subscribe(new Consumer<MediaStream>() {
+            @Override
+            public void accept(MediaStream mediaStream) {
+                if (mediaStream.isScreenPushing()) { // 正在推送，那取消推送。
+                    // 取消推送。
+                    mediaStream.stopPushScreen();
+                } else {    // 没在推送，那启动推送。
+                    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {     // lollipop 以前版本不支持。
+                        return;
+                    }
+                    MediaProjectionManager mMpMngr = (MediaProjectionManager) getApplicationContext().getSystemService(MEDIA_PROJECTION_SERVICE);
+                    startActivityForResult(mMpMngr.createScreenCaptureIntent(), REQUEST_MEDIA_PROJECTION);
+                    // 防止点多次.
+                    view.setEnabled(false);
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, final int resultCode, final Intent data) {
+        if (requestCode == REQUEST_MEDIA_PROJECTION) {
+            getMediaStream().subscribe(new Consumer<MediaStream>() {
+                @Override
+                public void accept(MediaStream mediaStream) {
+                    mediaStream.pushScreen(resultCode, data, HOST, "554", "screen111");
+                }
+            });
+        }
+    }
+
+    public void onSwitchCamera(View view) {
+        getMediaStream().subscribe(new Consumer<MediaStream>() {
+            @Override
+            public void accept(MediaStream mediaStream) throws Exception {
+                mediaStream.switchCamera();
+            }
+        });
+    }
+
+    public void onUVCCamera(View view) {
+        Intent intent = new Intent(this, UVCActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ EasyLib/myapplication/src/main/java/com/example/myapplication/UVCActivity.java
@@ -0,0 +1,209 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.SurfaceTexture;
+import android.os.Bundle;
+import android.os.Environment;
+import android.preference.PreferenceManager;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.lifecycle.Observer;
+
+import org.easydarwin.push.MediaStream;
+
+import io.reactivex.Single;
+import io.reactivex.functions.Consumer;
+
+public class UVCActivity extends AppCompatActivity {
+    private MediaStream mediaStream;
+
+    private static final int REQUEST_CAMERA_PERMISSION = 1000;
+    private Single<MediaStream> getMediaStream() {
+        Single<MediaStream> single = RxHelper.single(MediaStream.getBindedMediaStream(this, this), mediaStream);
+        if (mediaStream == null) {
+            return single.doOnSuccess(new Consumer<MediaStream>() {
+                @Override
+                public void accept(MediaStream ms) throws Exception {
+                    mediaStream = ms;
+                }
+            });
+        } else {
+            return single;
+        }
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_uvc);
+
+        // 启动服务...
+        Intent intent = new Intent(this, MediaStream.class);
+        startService(intent);
+
+        getMediaStream().subscribe(new Consumer<MediaStream>() {
+            @Override
+            public void accept(final MediaStream ms) throws Exception {
+
+                final TextView pushingStateText = findViewById(R.id.pushing_state);
+                final TextView pushingBtn = findViewById(R.id.pushing);
+                ms.observePushingState(UVCActivity.this, new Observer<MediaStream.PushingState>() {
+
+                    @Override
+                    public void onChanged(@Nullable MediaStream.PushingState pushingState) {
+                        if (pushingState.screenPushing) {
+                            pushingStateText.setText("屏幕推送");
+                        } else {
+                            pushingStateText.setText("推送");
+
+                            if (pushingState.state > 0) {
+                                pushingBtn.setText("停止");
+                            } else {
+                                pushingBtn.setText("推送");
+                            }
+
+                        }
+                        pushingStateText.append(":\t" + pushingState.msg);
+                        if (pushingState.state > 0) {
+                            pushingStateText.append(pushingState.url);
+                        }
+
+                    }
+                });
+                TextureView textureView = findViewById(R.id.texture_view);
+                textureView.setSurfaceTextureListener(new SurfaceTextureListenerWrapper() {
+                    @Override
+                    public void onSurfaceTextureAvailable(SurfaceTexture surfaceTexture, int i, int i1) {
+                        ms.setSurfaceTexture(surfaceTexture);
+                    }
+                    @Override
+                    public boolean onSurfaceTextureDestroyed(SurfaceTexture surfaceTexture) {
+                        ms.setSurfaceTexture(null);
+                        return true;
+                    }
+                });
+
+
+                if (ActivityCompat.checkSelfPermission(UVCActivity.this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED ||
+                        ActivityCompat.checkSelfPermission(UVCActivity.this, android.Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(UVCActivity.this, new String[]{android.Manifest.permission.CAMERA, android.Manifest.permission.RECORD_AUDIO}, REQUEST_CAMERA_PERMISSION);
+                }
+            }
+        }, new Consumer<Throwable>() {
+            @Override
+            public void accept(Throwable throwable) throws Exception {
+                Toast.makeText(UVCActivity.this, "创建服务出错!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+    // 权限获取到了.
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CAMERA_PERMISSION: {
+                if (grantResults.length > 1
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                    getMediaStream().subscribe(new Consumer<MediaStream>() {
+                        @Override
+                        public void accept(MediaStream mediaStream) throws Exception {
+                            mediaStream.notifyPermissionGranted();
+                        }
+                    });
+                } else {
+                    // 没有获取到权限,退出....
+                    Intent intent = new Intent(this, MediaStream.class);
+                    stopService(intent);
+
+                    finish();
+                }
+                break;
+            }
+        }
+    }
+
+    public void onPush(View view) {
+
+        // 异步获取到MediaStream对象.
+        getMediaStream().subscribe(new Consumer<MediaStream>() {
+            @Override
+            public void accept(final MediaStream mediaStream) throws Exception {
+                // 判断当前的推送状态.
+                MediaStream.PushingState state = mediaStream.getPushingState();
+                if (state != null && state.state > 0) { // 当前正在推送,那终止推送和预览
+                    mediaStream.stopStream();
+                    mediaStream.closeCameraPreview();
+                }else{
+                    // switch 0表示后置,1表示前置,2表示UVC摄像头
+                    RxHelper.single(mediaStream.switchCamera(2), null).subscribe(new Consumer<Object>() {
+                        @Override
+                        public void accept(Object o) throws Exception {
+                            String id = PreferenceManager.getDefaultSharedPreferences(UVCActivity.this).getString("uvc-id", null);
+                            if (id == null) {
+                                double v = Math.random() * 1000;
+                                id = "uvc_" + (int) v;
+                                PreferenceManager.getDefaultSharedPreferences(UVCActivity.this).edit().putString("uvc-id", id).apply();
+                            }
+                            mediaStream.startStream("cloud.easydarwin.org", "554", id);
+                        }
+                    }, new Consumer<Throwable>() {
+                        @Override
+                        public void accept(final Throwable t) throws Exception {
+                            t.printStackTrace();
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(UVCActivity.this, "UVC摄像头启动失败.." + t.getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    public void onRecord(View view) {       // 开始或结束录像.
+        final TextView txt = (TextView) view;
+        getMediaStream().subscribe(new Consumer<MediaStream>() {
+            @Override
+            public void accept(MediaStream mediaStream) throws Exception {
+                if (mediaStream.isRecording()){ // 如果正在录像,那停止.
+                    mediaStream.stopRecord();
+                    txt.setText("录像");
+                }else { // 没在录像,开始录像...
+                    // 表示最大录像时长为30秒,30秒后如果没有停止,会生成一个新文件.依次类推...
+                    // 文件格式为test_uvc_0.mp4,test_uvc_1.mp4,test_uvc_2.mp4,test_uvc_3.mp4
+                    String path = getExternalFilesDir(Environment.DIRECTORY_MOVIES) + "/test_uvc.mp4";
+                    mediaStream.startRecord(path, 30000);
+
+                    final TextView pushingStateText = findViewById(R.id.pushing_state);
+                    pushingStateText.append("\n录像地址:" + path);
+                    txt.setText("停止");
+                }
+            }
+        });
+    }
+
+    public void onQuit(View view) {     // 退出
+        finish();
+
+        // 终止服务...
+        Intent intent = new Intent(this, MediaStream.class);
+        stopService(intent);
+    }
+
+    public void onBackground(View view) {   // 后台
+        finish();
+    }
+}
